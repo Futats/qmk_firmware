949f2fb5846b33613dc0c98c5b0fa8ddadf15b7f
Futats <mkyslinger@gmail.com>
2026-07-23 19:46:54 +0200

    Updated keymap better suited for python coding, with fixes for ; issue and broken alt-codes containing 4

--- keyboards/beekeeb/piantor/keymaps/futats/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/futats/keymap.c
@@ -29,8 +29,8 @@ enum keycode_aliases {
   HRM_ENT = LT(SYM, KC_ENT),
   HRM_TAB = LT(NUM, KC_TAB),
 
-  SY_PIPE = LGUI_T(KC_PIPE),
-  SY_PERC = LALT_T(KC_PERC),
+  SY_HASH = LGUI_T(KC_HASH),
+  SY_DQUO = LALT_T(KC_DQUO),
   SY_LT = LCTL_T(KC_LT),
   SY_EQL = LSFT_T(KC_EQL),
 
@@ -43,8 +43,8 @@ enum keycode_aliases {
   N_F6 = LALT_T(KC_F6),
   N_F7 = LCTL_T(KC_F7),
   N_F8 = LSFT_T(KC_F8),
-
-  N_COLN = RGUI_T(KC_COLN),
+  // Not using the numpad homerow mod for shift/4 because the shift on 4 dumps the Alt+code in buffer, leading to Alt+24 becoming Alt+4. Note that Alt+41-9 are unaffected. 
+  N_0 = RGUI_T(KC_P0),
   N_6 = RALT_T(KC_P6),
   N_5 = RCTL_T(KC_P5),
   N_4 = RSFT_T(KC_P4),
@@ -74,17 +74,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[SYM] = LAYOUT_split_3x5_3(
-        KC_GRV, 		KC_QUOT, 	LSFT(KC_QUOT), 	LSFT(KC_4), LSFT(KC_1),			LSFT(KC_EQL), 	LSFT(KC_LBRC), 	LSFT(KC_RBRC), 	LSFT(KC_SLSH), 	KC_PAST,
-		SY_PIPE, 		SY_PERC, 	SY_LT, 			SY_EQL, 	KC_GT,				KC_MINS, 		SY_LPRN, 		SY_RPRN, 		SY_UNDS, 		SY_COLN,			
-		LSFT(KC_GRV), 	LSFT(KC_3), LSFT(KC_6), 	LSFT(KC_7), KC_BSLS,			LSFT(KC_2), 	KC_LBRC, 		KC_RBRC, 		KC_PDOT, 		KC_PSLS,				
-									KC_TRNS, 		KC_TRNS, 	KC_TRNS,			KC_TRNS, 		KC_TRNS, 		KC_TRNS
+        KC_PIPE,    KC_QUOT, 	KC_PERC, 	KC_DLR,     KC_EXLM,			KC_PLUS, 	KC_LCBR, 	KC_RCBR, 	KC_QUES, 	KC_PAST,
+		SY_HASH,    SY_DQUO, 	SY_LT,      SY_EQL,     KC_GT,				KC_MINS, 	SY_LPRN,	SY_RPRN,	SY_UNDS,	SY_COLN,			
+		KC_TILD,    KC_GRV,     KC_AMPR,    KC_CIRC,    KC_BSLS,			KC_AT, 	    KC_LBRC,	KC_RBRC,	KC_PDOT,	KC_PSLS,				
+								KC_TRNS,    KC_TRNS, 	KC_TRNS,			KC_TRNS, 	KC_TRNS,	KC_TRNS
     ),
 
 	[NUM] = LAYOUT_split_3x5_3(
-		KC_F1, 	KC_F2, 	KC_F3, 	KC_F4, 	KC_HOME,						LSFT(KC_EQL), 	KC_P7, 	KC_P8, 	KC_P9, 	KC_PAST,
-		N_F5, 	N_F6,	N_F7,	N_F8, 	KC_END,							KC_PMNS, 		N_4, 	N_5, 	N_6, 	N_COLN,
-		KC_F9, 	KC_F10, KC_F11, KC_F12, KC_CALC,						KC_P0, 			KC_P1,	KC_P2,	KC_P3,	KC_PSLS,
-						KC_LSFT, KC_LCTL, KC_LOPT,						KC_PDOT, 		KC_P0, 	KC_PDOT
+		KC_F1, 	KC_F2, 	KC_F3, 	KC_F4, 	KC_HOME,						KC_PLUS, KC_P7, KC_P8, 	KC_P9, 	KC_PAST,
+		N_F5, 	N_F6,	N_F7,	N_F8, 	KC_END,							KC_PMNS, N_4,   N_5, 	N_6, 	N_0,
+		KC_F9, 	KC_F10, KC_F11, KC_F12, KC_CALC,						KC_COLN, KC_P1, KC_P2,	KC_P3,	KC_PSLS,
+						KC_TRNS,KC_TRNS,KC_TRNS,						KC_PDOT, KC_P0, KC_PDOT
 	),
 
 	[NAV] = LAYOUT_split_3x5_3(
@@ -115,16 +115,40 @@ combo_t key_combos[] = {
 };
 // clang-format on
 
-// We can't send non-basic taps by default, so we need to intercept our complex 
-// tap-hold keys and send the more sophisticated keys here
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // 1. Intercept N_4 if ANY Alt state is active to prevent the Shift-hijack
+    if (keycode == RSFT_T(KC_P4)) {
+        if (get_mods() & (MOD_BIT(KC_LALT) | MOD_BIT(KC_RALT))) {
+            if (record->event.pressed) {
+                register_code(KC_P4);
+            } else {
+                unregister_code(KC_P4);
+            }
+            return false;
+        }
+    }
+
+    // 2. Intercept the pinky key collision based on active LAYER status
+    if (record->tap.count && record->event.pressed) {
+        // If they have the identical 16-bit footprint, look at the active layer
+        if (keycode == HRM_SCLN || keycode == SY_COLN) {
+            if (IS_LAYER_ON(SYM)) {
+                tap_code16(KC_COLN); // Force a clean Colon on your Symbol Layer
+            } else {
+                tap_code16(KC_SCLN); // Force a clean Semicolon on your Base Layer
+            }
+            return false; // Bypass default handling entirely
+        }
+    }
+
+    // 3. Original symbol tap-hold interceptions
     if (record->tap.count && record->event.pressed) {
         switch (keycode) {
-            case SY_PIPE: tap_code16(KC_PIPE);  return false;
-            case SY_PERC: tap_code16(KC_PERC);  return false;
+            case SY_HASH: tap_code16(KC_HASH);  return false;
+            case SY_DQUO: tap_code16(KC_DQUO);  return false;
             case SY_LT:   tap_code16(KC_LT);    return false;
             case SY_EQL:  tap_code16(KC_EQL);   return false;
-            case SY_COLN: tap_code16(KC_COLN);  return false;
             case SY_UNDS: tap_code16(KC_UNDS);  return false;
             case SY_RPRN: tap_code16(KC_RPRN);  return false;
             case SY_LPRN: tap_code16(KC_LPRN);  return false;
@@ -135,4 +159,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool get_combo_must_tap(uint16_t combo_index, combo_t *combo) {
     return true; // all combos are tap-only
+}
+
+bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case RSFT_T(KC_P4):
+            // Disable speculative hold for N_4 to protect Windows Alt-codes
+            return false;
+        default:
+            // Maintain the default behavior (True for Shift/Ctrl mod-taps)
+            return (QK_MOD_TAP_GET_MODS(keycode) & (MOD_LALT | MOD_LGUI)) == 0;
+    }
 }
